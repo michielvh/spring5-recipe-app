0c0d0a50ca4eb17cf998b583bcfebb9375e0a0cd
Michiel Van Huyck <mvanhuyck@cibg.brussels>
2022-06-28 17:04:53 +0200

    Test MockMVC

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -9,10 +9,16 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * Created by jt on 6/17/17.
@@ -32,6 +38,15 @@ public class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+//status().isOk -> going to check if we get a 200 statuscode back from controller
+        // .view() checks if view returned is correct by name
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
